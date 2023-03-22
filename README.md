ac5feaf51f19a1bcceb0ccf5dbd1d200a5ddb4e4
Ravi Kumar <ravikumargrk@hotmail.com>
2023-03-23 03:59:21 +0530

    added graph function

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # kairos-REST-client
-KairosDB REST API client with some extra handy graph functions.
+
+A kairosDB REST API Client with some extra handy graph functions.
+You need [pandas](https://pypi.org/project/pandas/) to save files and [matplotlib](https://pypi.org/project/matplotlib/) to plot data.
+
+# set up:
+define these environment variables in your system:
+
+```
+KAIROSDB_AUTH_URL   :   Url for authentication  
+KAIROSDB_QUERY_URL  :   Url for kairos query api
+KAIROSDB_AUTH_USER  :   Authentication email/username
+KAIROSDB_AUTH_PSWD  :   Authentication password
+```
+
+# Usage:
+
+```
+# import
+import kairosClient
+```
+
+```
+# you can use this handy function to generate iso format timestamps relative to your time.
+kairosClient.getTimeStamp(-24*60*60)
+```
+
+```
+# get data 
+myData = kairosClient.downloader(myTags, startTimeISO, endTimeISO)
+```
+
+```
+# save data
+# kairosClient.save(myData, '.')
+```
+
+```
+# plot in a ipynb notebook
+# data: dict returned by kairosClient.downloader()
+# cols = list of columns in data to be plotted.
+
+plotTimeSeries(data:dict, cols:list=None)
+```
--- kairosClient.py
+++ kairosClient.py
@@ -1,3 +1,4 @@
+from pandas.plotting._matplotlib.style import get_standard_colors
 import requests # for kairos REST API
 import pandas as pd
 import os
@@ -20,55 +21,69 @@ auth_payload = {
 if None in [KAIROSDB_AUTH_URL, KAIROSDB_QUERY_URL, KAIROSDB_AUTH_USER, KAIROSDB_AUTH_PSWD]:
     raise RuntimeError("Your container/device does not contain environment variables needed to connect to kairosDB. Read [ReadMe.MD]")
 
-from datetime import datetime, timedelta
-def currentUTCTimeStamp(nowdt = datetime.now()):
-    dt = timedelta(seconds = nowdt.second, microseconds=nowdt.microsecond)
-    nowdt = (nowdt - dt)
-    return int(nowdt.timestamp()*1000)
+import datetime
+tzinfo = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
+tzOffset = tzinfo.utcoffset(datetime.datetime.utcnow()).seconds
+tzName = tzinfo.tzname(datetime.datetime.utcnow())
 
-def plot_multi(dataDict, cols=None, spacing=.1, title='', **kwargs):
+def getTimeStamp(seconds_offset_from_now:float=0):
+    """
+    Gets timestamp in ISO Format. 
 
-    data = pd.DataFrame.from_dict(dataDict)
-
-    from pandas.plotting._matplotlib.style import get_standard_colors
-
-    # Get default color style from pandas - can be changed to any other color list
-    if cols is None: cols = list(data.columns)
-    if len(cols) == 0: return
-    if '__timeStamp__' in cols:
-        data['__timeStamp__'] = pd.to_datetime(data['__timeStamp__'], unit='ms', utc=True)
-        cols.remove('__timeStamp__')
+    Default behaviour is to return current timestamp.\n
+    You can pass argument seconds_offset_from_now, 
+    positive number for future, negative number for past
+    to get timestamps offset by seconds_offset_from_now.
+    """
+    now = datetime.datetime.now(datetime.timezone.utc).astimezone()
+    if seconds_offset_from_now==0:
+        return now.isoformat()
     else:
-        print('No __timeStamp__ axis in data')
-        return None
+        return (now + datetime.timedelta(seconds=seconds_offset_from_now)).isoformat()
+
+# def plot_multi(dataDict, cols=None, spacing=.1, title='', **kwargs):
+
+#     data = pd.DataFrame.from_dict(dataDict)
+
+#     from pandas.plotting._matplotlib.style import get_standard_colors
+
+#     # Get default color style from pandas - can be changed to any other color list
+#     if cols is None: cols = list(data.columns)
+#     if len(cols) == 0: return
+#     if '__timeStamp__' in cols:
+#         data['__timeStamp__'] = pd.to_datetime(data['__timeStamp__'], unit='ms', utc=True)
+#         cols.remove('__timeStamp__')
+#     else:
+#         print('No __timeStamp__ axis in data')
+#         return None
     
-    colors = get_standard_colors(num_colors=len(cols))
+#     colors = get_standard_colors(num_colors=len(cols))
 
-    # First axis
-    # ax = data.loc[:, cols[0]].plot(label=cols[0], color=colors[0], **kwargs)
-    ax = data.plot.scatter('__timeStamp__', cols[0], label=cols[0], color=colors[0], **kwargs)
-    ax.set_title(title)
-    ax.set_xlabel('time')
-    ax.set_ylabel(ylabel=cols[0])
-    ax.get_legend().remove()
-    lines, labels = ax.get_legend_handles_labels()
+#     # First axis
+#     # ax = data.loc[:, cols[0]].plot(label=cols[0], color=colors[0], **kwargs)
+#     ax = data.plot.scatter('__timeStamp__', cols[0], label=cols[0], color=colors[0], **kwargs)
+#     ax.set_title(title)
+#     ax.set_xlabel('time')
+#     ax.set_ylabel(ylabel=cols[0])
+#     ax.get_legend().remove()
+#     lines, labels = ax.get_legend_handles_labels()
 
-    for n in range(1, len(cols)):
-        # Multiple y-axes
-        ax_new = ax.twinx()
-        ax_new.spines['right'].set_position(('axes', 1 + spacing * (n - 1)))
-        # data.loc[:, cols[n]].plot(ax=ax_new, label=cols[n], color=colors[n % len(colors)], **kwargs)
-        data.plot.scatter('__timeStamp__', cols[n], ax=ax_new, label=cols[n], color=colors[n % len(colors)], **kwargs)
-        ax_new.set_ylabel(ylabel=cols[n])
-        ax_new.get_legend().remove()
+#     for n in range(1, len(cols)):
+#         # Multiple y-axes
+#         ax_new = ax.twinx()
+#         ax_new.spines['right'].set_position(('axes', 1 + spacing * (n - 1)))
+#         # data.loc[:, cols[n]].plot(ax=ax_new, label=cols[n], color=colors[n % len(colors)], **kwargs)
+#         data.plot.scatter('__timeStamp__', cols[n], ax=ax_new, label=cols[n], color=colors[n % len(colors)], **kwargs)
+#         ax_new.set_ylabel(ylabel=cols[n])
+#         ax_new.get_legend().remove()
 
-        # Proper legend position
-        line, label = ax_new.get_legend_handles_labels()
-        lines += line
-        labels += label
+#         # Proper legend position
+#         line, label = ax_new.get_legend_handles_labels()
+#         lines += line
+#         labels += label
 
-    ax.legend(lines, labels, loc=0)
-    return ax
+#     ax.legend(lines, labels, loc=0)
+#     return ax
 
 # class kairosAgent():
 
@@ -144,8 +159,8 @@ def downloader(tagList:list, startTimeISO:str, endTimeISO:str, timeStepMs=60000)
     global KAIROSDB_QUERY_URL
 
     payload = {
-        "start_absolute": datetime.fromisoformat(startTimeISO).timestamp()*1000,
-        "end_absolute": datetime.fromisoformat(endTimeISO).timestamp()*1000,
+        "start_absolute": datetime.datetime.fromisoformat(startTimeISO).timestamp()*1000,
+        "end_absolute": datetime.datetime.fromisoformat(endTimeISO).timestamp()*1000,
         "time_zone" : "Asia/Kolkata",
         "metrics": []
     }        
@@ -206,8 +221,8 @@ def save(data:dict, rootPath='EXPORTED_DATA\\'):
     if '__timeStamp__' in cols:
         startTimeMs = data['__timeStamp__'][0]
         endTimeMs  = data['__timeStamp__'][-1]
-        startTimeStr = datetime.utcfromtimestamp(startTimeMs/1000).strftime('%Y-%m-%d-%H-%M')
-        endTimeStr   = datetime.utcfromtimestamp(endTimeMs/1000).strftime('%Y-%m-%d-%H-%M')
+        startTimeStr = datetime.datetime.utcfromtimestamp(startTimeMs/1000).strftime('%Y-%m-%d-%H-%M')
+        endTimeStr   = datetime.datetime.utcfromtimestamp(endTimeMs/1000).strftime('%Y-%m-%d-%H-%M')
         cols.remove('__timeStamp__')
     
     prefix = ''
@@ -221,9 +236,41 @@ def save(data:dict, rootPath='EXPORTED_DATA\\'):
     filename += f'DATA_{prefix}_({startTimeStr}--{endTimeStr}).csv'
 
     output = {
-            'time (IST)':[((t+19800000)/86400000)+25569 for t in data['__timeStamp__']]
+            f'time ({tzName})':[((t+(tzOffset*1000))/86400000)+25569 for t in data['__timeStamp__']]
         }
     output.update({key:data[key] for key in data if key != '__timeStamp__'})
     df = pd.DataFrame(output)
     df.to_csv(filename, index=False)
+    pass
+
+def plotTimeSeries(data:dict, cols:list=None):
+    
+    # Get default color style from pandas - can be changed to any other color list
+    if '__timeStamp__' not in data:
+        raise KeyError('No __timeStamp__ axis in data')
+    df = pd.DataFrame({key:data[key] for key in ['__timeStamp__']+cols})
+    df = df.drop(columns=['__timeStamp__'])
+    
+    df.index = pd.to_datetime(data['__timeStamp__'], unit='ms', utc=True).tz_convert(tzinfo)
+
+    if cols is None: 
+        cols = list(df.columns)
+    
+    colors = get_standard_colors(len(cols))
+    lines = []
+    labels = []
+    ax = None
+
+    for id, col in enumerate(cols):
+        if ax == None:
+            ax = df.plot(y=col, color=colors[id], marker='.', legend=False)
+        else:
+            ax = ax.twinx()
+            df.plot(y=col, ax=ax, color=colors[id], marker='.', legend=False)
+
+        line, label = ax.get_legend_handles_labels()
+        lines += line
+        labels += label
+    
+    ax.legend(lines, labels)
     pass
